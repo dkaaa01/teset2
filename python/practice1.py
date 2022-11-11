3fe4b06089560843858f6cd83b5875b47230a087
Mingyeom Kim <alsrua15050@naver.com>
2022-11-12 00:00:17 +0900

    test using  basic python file

---
+++ python/practice1.py
@@ -0,0 +1,16 @@
+import random
+
+list = []
+for i in range(5):
+    list.append(random.randint(1, 5))
+
+for i in range(5):
+    ans = int(input("<기회 {}번 남음> 1~5중에 아무 수나 골라보세요: ".format(5 - i)))
+    if ans == list[i]:
+        print("정답 !")
+        break
+    else:
+        print("오답..")
+        continue
+
+print("결과 =", list)
